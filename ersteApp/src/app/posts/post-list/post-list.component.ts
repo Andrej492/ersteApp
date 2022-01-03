a1b4cb95546a0ecf8f031245d99366fc909ea7c8
Andrej492 <andrejmatovic1@gmail.com>
2022-01-03 11:07:56 +0100

    Update: Binded list to create and added Post model

--- ersteApp/src/app/posts/post-list/post-list.component.ts
+++ ersteApp/src/app/posts/post-list/post-list.component.ts
@@ -1,5 +1,5 @@
-import { Component } from "@angular/core";
-
+import { Component, Input } from "@angular/core";
+import { Post } from '../post.model';
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
@@ -12,5 +12,5 @@ export class PostListComponent{
   //   {title: 'Third-Post', content: 'This is a 3rd post!'}
   // ];
 
-  posts = [];
+  @Input() posts: Post[] = [];
 }
